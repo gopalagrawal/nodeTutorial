26e762f5ca65bc4cfea2295d64735e19f94bfffc
gopalagrawal <ga.willbe@gmail.com>
2023-02-25 15:21:46 +0530

    Parsing json data in POST request body

--- src/app.js
+++ src/app.js
@@ -2,13 +2,52 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+// middleware to parse POST request body
+app.use(express.json());
+app.use(express.urlencoded( {extended:true} ));
+
+// "Quoted" keys with possibly spaces in there
+const jsonData = {
+    "name": "Gopal",
+    "industry": "Looking",
+    "favorite Colors": ["blue", "maroon", "purpule", "red", "lime"]
+}
+
+//Keys don't need "quotes" if no spaces. 
+const jsonData2 = {
+    name: "Gopal",
+    industry: "Looking",
+    favoriteColors: ["blue", "maroon", "purpule", "red", "lime"]
+}
+
+const customers = [
+	{
+		"name": "Caleb",
+		"industry": "music"	
+	},
+	{
+		"name": "John",
+		"industry": "networking"	
+	},
+	{
+		"name": "Sal",
+		"industry": "sports medicine"	
+	}
+];
+
 // Set up routes
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send({"name": jsonData.name, "colors": jsonData2["favoriteColors"]})
 })
 
-app.post('/', (req, res) => {
-    res.send('Got a POST request')
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers})
+})
+
+app.post('/api/customers', (req, res) => {
+    console.log("Received: " + JSON.stringify(req.body) )
+    customers.push(req.body)
+    res.send({"customers": customers})
 })
 
 
